3d1c03dea5186901cbb511589d36ed2b917e4ecf
nazhG <nazhirgonzalez@gmail.com>
2024-12-22 08:29:00 -0500

    diag disply when player step in

--- area_diag.gd
+++ area_diag.gd
@@ -1,12 +1,13 @@
 extends Area3D
 
-@onready var diag = get_parent().get_node("area_diag")
+@onready var diag = get_parent().get_node("boss_diag")
 
 func _ready() -> void:
 	# Conecta la señal body_entered para detectar la entrada del jugador
 	self.body_entered.connect(_on_player_enter)
 
 func _on_player_enter(body) -> void:
-	print('entrar', diag)
+	print('entrar ', body.name)
 	if body.name == "Player":  # Verifica si el jugador entra
+		print('mostrando dialogo', diag.visible)
 		diag.visible = true  # Activa el CanvasLayer para mostrar los diálogos
