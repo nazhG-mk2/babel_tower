90cf68307752a2b7b8ccbd244d992f7b47418cde
nazhG <nazhirgonzalez@gmail.com>
2025-03-20 19:04:29 -0500

    fix

--- scripts/barrel_bleakable.gd
+++ scripts/barrel_bleakable.gd
@@ -1,5 +1,13 @@
 extends Area3D
 
+@export var bronken_model:PackedScene;
+
+func _ready():
+	body_entered.connect(_on_body_entered)
+
 func _on_body_entered(body):
 	if body.is_in_group("weapon"):
-		queue_free()  # Destruir el proyectil
+		var broken_inst:Node3D = bronken_model.instantiate();
+		self.get_parent().get_parent().add_child(broken_inst)
+		broken_inst.transform = self.get_parent().transform
+		self.get_parent().queue_free()  # Destruir el proyectil
