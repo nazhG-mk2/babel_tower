425db947d3731dd83fd67c4d8ad0091226b6eaaf
nazhG <nazhirgonzalez@gmail.com>
2025-04-01 19:45:37 -0500

    2d

--- scripts/player_2D.gd
+++ scripts/player_2D.gd
@@ -1,6 +1,14 @@
 extends CharacterBody2D
 
 @export var speed: float = 200.0
+@export var radius = 20
+@onready var hitbox = $HitBox
+
+func _ready() -> void:
+	hitbox.shape.radius = radius
+
+func _draw():
+	draw_circle(self.position, radius, Color(1, 0, 0, .5))  # Círculo rojo en (100,100) con radio 50
 
 func _physics_process(_delta):
 	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
